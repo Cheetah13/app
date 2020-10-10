c207c1b48f23d2b78e521d3e45cd2391e8c71f62
Rayyan <rayyansaidi@outlook.com>
2020-10-10 19:44:42 -0400

    Fix JSX style prop

--- src/Content.js
+++ src/Content.js
@@ -21,7 +21,7 @@ function Content (props) {
       </ContentParagraph>
       {
         process.versions.electron
-        ? null : (<div><a href="download_files/wild-cats.dmg"><button style="border-radius: 20px;">Download the snap for linux</button></a><br /><br /><a href="download_files/wild-cats.exe"><button style="border-radius: 20px;">Download the exe for Windows</button></a><br /><br /><a href="download_files/wild-cats.dmg"><button style="border-radius: 20px;">Download the dmg for Mac!</button></a></div>)
+        ? null : (<div><a href="download_files/wild-cats.dmg"><button style={{ borderRadius: '20px' }}>Download the snap for linux</button></a><br /><br /><a href="download_files/wild-cats.exe"><button style={{ borderRadius: '20px' }}>Download the exe for Windows</button></a><br /><br /><a href="download_files/wild-cats.dmg"><button style={{ borderRadius: '20px' }}>Download the dmg for Mac!</button></a></div>)
       }
       <Button
         secondary
